3b5a3df47fc48603d0c5295704c7cfdfc44b6913
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-06-25 12:18:27 +0700

    Update hash tag pattern to allow new #"tag tag" syntax
    - we can now add tags with spaces as search filters as long as they are surrounded with quotation marks
    - tests in memex ext repo

--- ts/storage/constants.ts
+++ ts/storage/constants.ts
@@ -1,6 +1,7 @@
 /**
- * Pattern to match hashtag prefix syntax for tags.
- * NOTE: Please update these together if they change.
+ * NOTE: These regexps contain the same base pattern.
+ *      Please update these together if they change.
+ * TODO: Can we share the base pattern somehow? `RegExp` constructor?
  */
-export const HASH_TAG_PATTERN = /^-?#\w+([-. ]\w+)*$/
+export const HASH_TAG_PATTERN = /^-?#\"\w+([-. ]\w+)*\"$|^-?#\w+([-. ]\w+)*$/
 export const VALID_TAG_PATTERN = /^\w+([-. ]\w+)*$/
