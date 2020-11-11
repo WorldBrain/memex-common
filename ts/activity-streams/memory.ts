d83f1b9dc394b44848ce786effcca36422454b2c
Vincent den Boer <vincent@youapt.eu>
2020-11-11 11:29:00 +0100

    Implement minimal memory activity stream

--- ts/activity-streams/memory.ts
+++ ts/activity-streams/memory.ts
@@ -1,5 +1,43 @@
-import { ActivityStreamsService, ActivityStream } from "./types";
+import { ActivityStreamsService, ActivityStream, NotificationStreamResult, ActivityRequest, EntitityActivities } from "./types";
 
-export class MemoryStreamsService {
+export interface MemoryFollow {
+    source: { type: string, id: string | number }
+    target: { type: string, id: string | number }
+    feeds: { user: boolean, notification: boolean }
+}
+export interface MemoryActivity {
+    source: { type: string, id: string | number }
+    type: string
+    data: any
+}
 
+export default class MemoryStreamsService {
+    follows: MemoryFollow[] = []
+    activities: MemoryActivity[] = []
+
+    constructor(private options: {
+        getCurrentUserId(): Promise<number | string | null | undefined>
+    }) { }
+
+    followEntity: ActivityStreamsService['followEntity'] = async <EntityType extends keyof ActivityStream>(params: {
+        entityType: EntityType
+        entity: ActivityStream[EntityType]['entity']
+        feeds: { user: boolean, notification: boolean }
+    }): Promise<void> => {
+        this.follows.push({
+            source: { type: 'user', id: await this.options.getCurrentUserId()! },
+            target: { type: params.entityType, id: params.entity.id },
+            feeds: params.feeds
+        })
+    }
+
+    addActivity: ActivityStreamsService['addActivity'] = async <EntityType extends keyof ActivityStream, ActivityType extends keyof EntitityActivities<EntityType>>(params: {
+        entityType: EntityType
+        entity: ActivityStream[EntityType]['entity'],
+    } & ActivityRequest<EntityType, ActivityType>): Promise<void> => {
+    }
+
+    async getNotifications(): Promise<Array<NotificationStreamResult<keyof ActivityStream>>> {
+        return []
+    }
 }
