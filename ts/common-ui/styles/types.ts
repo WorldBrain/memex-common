559d424c19cad06909f378c1527e49e09d46d929
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2020-12-01 12:43:03 +0700

    Add theme typings from Memex Social

---
+++ ts/common-ui/styles/types.ts
@@ -0,0 +1,26 @@
+import { SpacingValue } from 'styled-components-spacing'
+
+export type ColorThemeKeys =
+  | 'background'
+  | 'warning'
+  | 'primary'
+  | 'secondary'
+  | 'subText'
+  | 'grey'
+  | 'black'
+
+export type FontThemeKeys = 'primary' | 'secondary'
+
+export type FontSizeThemeKeys = 'listTitle' | 'url'
+
+export interface MemexTheme {
+  spacings: { [Key in SpacingValue]: string }
+  colors: { [Key in ColorThemeKeys]: string } & {
+    overlay: { [Key in 'background' | 'dialog']: string }
+  }
+  fonts: { [Key in FontThemeKeys]: string }
+  fontSizes: { [Key in FontSizeThemeKeys]: string }
+  hoverBackgrounds: { [Key in 'primary']: string }
+  borderRadii: { [Key in 'default']: string }
+  zIndices: { [Key in 'overlay']: number }
+}
