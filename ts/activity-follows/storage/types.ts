29ec39191478b1e71688156c6cdd95142071b633
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-01-20 14:23:06 +0700

    Fix path issues messing up Memex Social setup
    - Memex Social doesn't like absolute paths from `src`
    - resulted in it not being able to find the types

--- ts/activity-follows/storage/types.ts
+++ ts/activity-follows/storage/types.ts
@@ -1,6 +1,6 @@
 import { AutoPkStorageReference } from "../../storage/references";
 
-export { ActivityFollow } from "src/web-interface/types/storex-generated/activity-follows";
+export { ActivityFollow } from "../../web-interface/types/storex-generated/activity-follows";
 
 export type ActivityFollowReference = AutoPkStorageReference<
   "activity-follow-reference"
