0f7979c219d80faf48248030e2d26b21bbb033c9
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-03-19 19:46:15 +0700

    Merge new item-box changes w/ renderCreationInfo

--- ts/common-ui/components/creation-info.tsx
+++ ts/common-ui/components/creation-info.tsx
@@ -1,61 +1,68 @@
 import moment from 'moment'
 import React from 'react'
 import styled from 'styled-components'
+import { Margin } from 'styled-components-spacing'
+import UserAvatar from './user-avatar'
 import { User } from '../../web-interface/types/users'
 
 const StyledCreationInfo = styled.div`
     display: flex;
 `
 
-// const AvatarHolder = styled.div`
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: center;
-// `
-
-const Details = styled.div`
+const AvatarHolder = styled.div`
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    justify-content: center;
 `
 
+const Details = styled.div``
 const Creator = styled.div`
     font-weight: 600;
     color: ${(props) => props.theme.colors.primary};
     font-size: 12px;
-    max-width: 150px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+`
+const EditedText = styled.span`
+    font-weight: bold;
+    margin-right: 5px;
 `
 const CreationDate = styled.div`
-    font-family: 'Poppins';
+    font-family: 'Poppins', sans-serif;
     font-weight: normal;
-    font-size: 11px;
+    font-size: 12px;
     color: ${(props) => props.theme.colors.primary};
-    padding-left: 10px;
 `
 
 export interface CreationInfoProps {
     createdWhen?: number
+    lastEdited?: number
     creator?: Pick<User, 'displayName'> | null
 }
 
 export default function CreationInfo(props: CreationInfoProps) {
     return (
         <StyledCreationInfo>
-            {/*<AvatarHolder>
-        <Margin right="small">
-          <UserAvatar user={props.creator} loading={!props.creator} />
-        </Margin>
-      </AvatarHolder>
-    */}
+            {props.creator != null && (
+                <AvatarHolder>
+                    <Margin right="small">
+                        <UserAvatar
+                            user={props.creator}
+                            loading={!props.creator}
+                        />
+                    </Margin>
+                </AvatarHolder>
+            )}
             <Details>
-                <Creator>
-                    {props.creator?.displayName ?? <span>&nbsp;</span>}
-                </Creator>
+                {props.creator?.displayName && (
+                    <Creator>
+                        {props.creator?.displayName ?? <span>&nbsp;</span>}
+                    </Creator>
+                )}
                 <CreationDate>
-                    {props.createdWhen ? (
-                        moment(props.createdWhen).format('LLL')
+                    {props.lastEdited && <EditedText>Last Edit: </EditedText>}
+                    {props.createdWhen || props.lastEdited ? (
+                        moment(props.lastEdited ?? props.createdWhen).format(
+                            'LLL',
+                        )
                     ) : (
                         <span>&nbsp;</span>
                     )}
--- ts/common-ui/components/item-box-bottom.tsx
+++ ts/common-ui/components/item-box-bottom.tsx
@@ -1,59 +1,75 @@
-import React from 'react'
+import React, { HTMLProps } from 'react'
 import styled from 'styled-components'
-import { Margin } from 'styled-components-spacing'
+import Margin from '../components/Margin'
 import CreationInfo, { CreationInfoProps } from './creation-info'
+import ButtonTooltip from './button-tooltip'
 
 const Bottom = styled.div`
     display: flex;
     flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0px 15px 0px 15px;
-    border-top: 1px solid #e0e0e0;
+    padding: 0 15px;
     height: 40px;
+    align-items: center;
+    border-top: 1px solid #e0e0e0;
 `
 
 const Actions = styled.div`
     display: flex;
     flex-grow: 2;
-    align-items: flex-end;
+    align-items: center;
     justify-content: flex-end;
 `
-const Action = styled.div<{ image: string }>`
+const Action = styled.div<{ image: string; isDisabled?: boolean }>`
     display: block;
     width: 20px;
-    height: 20px;
-    cursor: pointer;
-    background-image: url('${(props) => props.image}');
-    background-size: contain;
+    height: 20px !important;
+    opacity: ${(props) => (props.isDisabled ? 0.35 : 1)};
+    cursor: ${(props) => (props.isDisabled ? 'default' : 'pointer')};
+    background-image: url("${(props) => props.image}");
+    background-size: auto 14px;
     background-position: center center;
     background-repeat: no-repeat;
 `
 
+export type ItemBoxBottomAction =
+    | {
+          key: string
+          image: string
+          isDisabled?: boolean
+          tooltipText?: string
+          onClick?: React.MouseEventHandler<HTMLDivElement>
+      }
+    | null
+    | false
+    | undefined
+
 export interface ItemBoxBottomProps {
     creationInfo: CreationInfoProps
     replyCount?: number
-    actions?: Array<
-        | { key: string; image: string; onClick?(): void }
-        | null
-        | false
-        | undefined
-    >
+    firstDivProps?: HTMLProps<HTMLDivElement>
+    actions?: Array<ItemBoxBottomAction>
     renderCreationInfo?: (props: { children: React.ReactNode }) => React.ReactNode
 }
 
 export default function ItemBoxBottom(props: ItemBoxBottomProps) {
-    const renderCreationInfo = props.renderCreationInfo ?? ((props) => props.children)
-
     return (
-        <Bottom>
-            {renderCreationInfo({ children: <CreationInfo {...props.creationInfo} /> })}
+        <Bottom {...(props.firstDivProps ?? {})}>
+            {props.renderCreationInfo({ children: <CreationInfo {...props.creationInfo} /> })}
             <Actions>
                 {props.actions?.map?.(
                     (actionProps) =>
                         actionProps && (
-                            <Margin key={actionProps.key} left="small">
-                                <Action {...actionProps} />
+                            <Margin key={actionProps.key} left="5px">
+                                {actionProps.tooltipText ? (
+                                    <ButtonTooltip
+                                        position="bottom"
+                                        tooltipText={actionProps.tooltipText}
+                                    >
+                                        <Action {...actionProps} />
+                                    </ButtonTooltip>
+                                ) : (
+                                    <Action {...actionProps} />
+                                )}
                             </Margin>
                         ),
                 )}
