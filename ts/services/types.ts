f9c7872308f5a0789859f6e5f15c02e09cbc485b
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-03-25 08:44:37 +0700

    Bring over Overlay from Memex Social

--- ts/services/types.ts
+++ ts/services/types.ts
@@ -3,6 +3,12 @@ import type { OverlayServiceInterface } from './overlay/types'
 import type { ClipboardServiceInterface } from './clipboard/types'
 import type { LogicRegistryServiceInterface } from './logic-registry/types'
 
+type UIServices = 'logicRegistry' | 'device'
+export type UIElementServices<Wanted extends keyof Services = never> = Pick<
+    Services,
+    UIServices | Wanted
+>
+
 export interface Services {
     overlay: OverlayServiceInterface
     clipboard: ClipboardServiceInterface
