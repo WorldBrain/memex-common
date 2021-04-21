27dfe03a737fdac2a4f0fd03fa102f2ba531cd6c
Vincent den Boer <vincent@youapt.eu>
2021-04-21 12:54:18 +0200

    Theoretical implementation of application layer client

---
+++ ts/firebase-backend/app-layer/client.ts
@@ -0,0 +1,16 @@
+import { FirebaseApplicationLayer } from "./types";
+import { APPLICATION_LAYER_METHOD_NAMES } from "./constants";
+
+type ExecuteCall = (name: string, args: any) => Promise<any>;
+
+export function createClientApplicationLayer(executeCall: ExecuteCall): FirebaseApplicationLayer {
+    const layer: { [methodName: string]: (args: any) => Promise<any> } = {}
+    for (const methodName of Object.keys(APPLICATION_LAYER_METHOD_NAMES)) {
+        layer[methodName] = createMethod(executeCall, methodName)
+    }
+    return layer as any
+}
+
+function createMethod(executeCall: ExecuteCall, methodName: string) {
+    return (methodArgs: any) => executeCall(methodName, methodArgs)
+}
