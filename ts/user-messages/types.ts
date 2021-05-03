a3df03bd5eeb491e6ee2b519164ede9ce1acef27
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2021-05-03 10:39:21 +0700

    Add created annotation user message type

--- ts/user-messages/types.ts
+++ ts/user-messages/types.ts
@@ -1,5 +1,11 @@
-export type UserMessage = JoinedSharedListMessage
+export type UserMessage = JoinedSharedListMessage | CreatedAnnotationMessage
+
 export interface JoinedSharedListMessage {
     type: 'joined-collection'
     sharedListId: number | string
+}
+
+export interface CreatedAnnotationMessage {
+    type: 'created-annotation'
+    sharedAnnotationId: number | string
 }
